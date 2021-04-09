9210b42f3ddb583f26e9db27838f30e6a87f52e1
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-09 14:01:52 +0200

    add server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const http = require("http");
+const socket = require("./features/websockets");
+require("dotenv").config();
+
+// server
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
+
+// use public folder for static files
+app.use(express.static("public"))
+	.disable("x-powered-by");
+
+// socket implementation
+socket(server); 
+
+server.listen(PORT, function () {
+	console.log(`Listening on ${PORT}`);
+});
