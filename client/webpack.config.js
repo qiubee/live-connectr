e2b946ad86ed3030538898b3b409bae2f764d3e3
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-14 16:52:22 +0200

    add webpack config

---
+++ client/webpack.config.js
@@ -0,0 +1,79 @@
+const { VueLoaderPlugin } = require("vue-loader");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+	entry: {
+		main: "./src/main.js",
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "/",
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.vue$/,
+				loader: "vue-loader",
+			},
+			{
+				test: /\.css$/,
+				use: [
+					process.env.NODE_ENV === "production" ? 
+					"style-loader" : 
+					MiniCssExtractPlugin.loader, "css-loader"
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin(),
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "public", "index.html"),
+			filename: "index.html"
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'index.css'
+		}),
+		new CleanWebpackPlugin()
+	],
+	optimization: {
+		minimize: false,
+		minimizer: [
+			new CssMinimizerPlugin()
+		]
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		hot: true,
+		stats: "minimal",
+		inline: true,
+		port: 5000,
+	},
+	resolve: {
+		alias: {
+			vue: "@vue/runtime-dom",
+			"@": path.resolve(__dirname, "src")
+		},
+		extensions: ["*", ".js", ".vue", ".json"],
+	}
+};
