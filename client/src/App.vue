fd1722419f9d48db1a16df40532d29752f8dc1f3
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-14 16:53:20 +0200

    add vue app

---
+++ client/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+	<div id="nav">
+		<router-link to="/">Home</router-link> |
+		<router-link to="/about">About</router-link>
+	</div>
+	<router-view/>
+</template>
+
+<style>
+@font-face {
+    font-family: Tommy Soft;
+    font-weight: 400;
+    src: url("assets/fonts/tommy-soft-regular.otf") format("opentype");
+}
+
+@font-face {
+    font-family: Tommy Soft;
+    font-weight: 500;
+    src: url("assets/fonts/tommy-soft-medium.otf") format("opentype");
+}
+
+@font-face {
+    font-family: Tommy Soft;
+    font-weight: 700;
+    src: url("assets/fonts/tommy-soft-bold.otf") format("opentype");
+}
+
+@font-face {
+    font-family: Tommy Soft;
+    font-weight: 900;
+    src: url("assets/fonts/tommy-soft-black.otf") format("opentype");
+}
+
+body, body * {
+	margin: 0;
+    padding: 0;
+    font-family: Tommy Soft, Avenir, Helvetica, Roboto, Ubuntu, Arial, sans-serif;
+}
+
+body {
+	background-color: white;
+}
+
+ul, ol {
+	list-style-type: none;
+}
+
+a {
+	text-decoration: none;
+}
+
+#nav {
+	padding: 30px;
+}
+
+#nav a {
+	font-weight: bold;
+	color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+	color: #42b983;
+}
+</style>
