d746286919279dd302f98c3c210925d662c460b6
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-15 16:48:57 +0200

    allow localhost origin

--- server/features/websockets.js
+++ server/features/websockets.js
@@ -1,5 +1,9 @@
 module.exports = function (server) {
-	const io = require("socket.io")(server);
+	const io = require("socket.io")(server, {
+		cors: {
+			origin: "http://localhost:5000",
+		}
+	});
 	io.on("connection", function (socket) {
 		console.log("client connected");
 		socket.send("connected");
