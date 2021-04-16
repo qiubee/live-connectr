f7e580c19c33d3f778be3806a4352a6d55733f9c
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-16 18:39:05 +0200

    fetch messages of room

--- server/features/websockets.js
+++ server/features/websockets.js
@@ -1,3 +1,5 @@
+const db = require("../modules/database");
+
 module.exports = function (server) {
 	const io = require("socket.io")(server, {
 		cors: {
@@ -7,10 +9,23 @@ module.exports = function (server) {
 	io.on("connection", function (socket) {
 		console.log("client connected");
 		
-		socket.on("messages", function () {
-			// 1. get room id
-			// 2. fetch messages
-			// 3. emit messages
+		socket.on("join room", function (room) {
+			socket.join(room);
+		});
+
+		socket.on("add message", function (data) {
+			console.log(data);
+		});
+
+		socket.on("messages", function (roomId) {
+			const chatId = db.getOne("rooms", function (room) {
+				return room.id === roomId;
+			}).chatId;
+			const messages = db.getOne("chats", function (chat) {
+				return chat.id === chatId;
+			}).messages;
+			// socket.to(room.name).emit("messages", messages);
+			socket.emit("messages", messages);
 		});
 
 		socket.on("disconnect", function () {
