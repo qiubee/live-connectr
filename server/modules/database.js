e93a7b884287ab4afb0c60f814996e247b3fe807
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-16 18:39:40 +0200

    add database.js & add db methods

---
+++ server/modules/database.js
@@ -0,0 +1,59 @@
+const { readJSON, writeToJSON} = require("./file");
+const { currentDateTime } = require("./date");
+require("dotenv").config();
+
+const FOLDER = process.env.DB_FOLDER || "data";
+
+function addToDatabase(name, data) {
+	JSON.stringify(writeToJSON(`${FOLDER}/` + name + ".json", data));
+	console.log(currentDateTime() + `: Data added to: ${FOLDER}/${name}.json`);
+}
+
+function readFromDatabase(name) {
+	console.log(currentDateTime() + `: Data loaded from: ${FOLDER}/${name}.json`);
+	return readJSON(`${FOLDER}/` + name + ".json");
+}
+
+
+// methods for interacting with database
+
+function insertOne(name, data) {
+	const collection = readFromDatabase(name);
+	const newCollection = collection.push(data);
+	addToDatabase(name, newCollection);
+}
+
+function deleteOne(name, data) {
+	const collection = readFromDatabase(name);
+	const newCollection = collection.filter(function (item) {
+		return item !== data;
+	});
+	addToDatabase(name, newCollection);
+}
+
+function deleteMany(name, query) {
+	const collection = readFromDatabase(name);
+	const newCollection = collection.filter(query);
+	addToDatabase(name, newCollection);
+}
+
+function update(name, filterQuery, updateQuery) {
+	const collection = readFromDatabase(name);
+	const newCollection = collection.map(filterQuery, updateQuery);
+	addToDatabase(name, newCollection);
+}
+
+function getOne(name, filterQuery) {
+	return readFromDatabase(name).find(filterQuery);
+}
+
+function getMany(name, filterQuery) {
+	return readFromDatabase(name).filter(filterQuery);
+}
+
+exports.insertOne = insertOne;
+exports.deleteOne = deleteOne;
+exports.deleteMany = deleteMany;
+exports.update = update;
+exports.getOne = getOne;
+exports.getMany = getMany;
