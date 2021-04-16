1697a62e8a7601424c553f86fa51ba0fb97968ed
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-16 15:20:24 +0200

    add ChatMessage component

---
+++ client/src/components/ChatMessage.vue
@@ -0,0 +1,38 @@
+<template>
+	<li>
+		<div class="message">
+			<div>
+				<p>{{message}}</p>
+			</div>
+			<div>
+				<span>{{user.name}}</span>
+				<span>
+					<time v-bind:datetime="datetime">{{timeString}}</time>
+				</span>
+			</div>
+		</div>
+	</li>
+</template>
+
+<script>
+export default {
+	name: "ChatMessage",
+	props: {
+		user: Object,
+		socket: Object,
+		message: String,
+		datetime: String
+	},
+	data() {
+		return {
+			timeString: ""
+		};
+	},
+	mounted() {
+	}
+};
+</script>
+
+<style>
+
+</style>
