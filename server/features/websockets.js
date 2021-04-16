6e313406bfb32bcaebf618fb3ee32360fea35ea8
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-16 15:23:17 +0200

    add message socket listener

--- server/features/websockets.js
+++ server/features/websockets.js
@@ -6,7 +6,12 @@ module.exports = function (server) {
 	});
 	io.on("connection", function (socket) {
 		console.log("client connected");
-		socket.send("connected");
+		
+		socket.on("messages", function () {
+			// 1. get room id
+			// 2. fetch messages
+			// 3. emit messages
+		});
 
 		socket.on("disconnect", function () {
 			console.log("client disconnected");
