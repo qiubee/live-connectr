9cfc8d0c219166ee383364e6df3a618a50934cd3
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-16 15:20:39 +0200

    add Room component

---
+++ client/src/components/Room.vue
@@ -0,0 +1,38 @@
+<template>
+  <Chat :socket="socket"/>
+</template>
+
+<script>
+import Chat from "@/components/Chat.vue";
+import io from "socket.io-client";
+
+export default {
+	name: "Room",
+	components: {
+		Chat
+	},
+	data() {
+		return {
+			socket: Object
+		};
+	},
+	created() {
+		// const host = location.origin.replace(/^http/, "ws");
+		const socket = io("http://localhost:8000");
+
+		socket.on("connect", function () {
+			console.log("connected");
+		});
+
+		socket.on("disconnect", function () {
+			console.log("disconnected");
+		});
+
+		this.socket = socket;
+	}
+};
+</script>
+
+<style>
+
+</style>
