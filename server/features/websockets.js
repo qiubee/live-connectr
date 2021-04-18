04bc7e54d5e34bb565a192bc8bba08894ee76f59
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-18 20:25:00 +0200

    add message to db & update retrieve messages

--- server/features/websockets.js
+++ server/features/websockets.js
@@ -6,30 +6,63 @@ module.exports = function (server) {
 			origin: "http://localhost:5000",
 		}
 	});
+	const clients = [];
 	io.on("connection", function (socket) {
-		console.log("client connected");
-		
+		// add client to clients list
+		clients.push({
+			id: socket.id,
+		});
+		console.log("client connected",`(total: ${clients.length})`);
+
 		socket.on("join room", function (room) {
 			socket.join(room);
 		});
 
-		socket.on("add message", function (data) {
-			console.log(data);
+		socket.on("add message", function (message) {
+			db.insertOne("messages", message);
+			const user = db.getOne("users", function (user) {
+				return user.id === message.userId;
+			});
+			message.user = user;
+			delete message.userId;
+			socket.emit("new message", message);
 		});
 
 		socket.on("messages", function (roomId) {
-			const chatId = db.getOne("rooms", function (room) {
+			// find users of room
+			const roomMembers = db.getOne("rooms", function (room) {
 				return room.id === roomId;
-			}).chatId;
-			const messages = db.getOne("chats", function (chat) {
-				return chat.id === chatId;
-			}).messages;
+			}).users;
+			const users = db.getMany("users", function (user) {
+				for (const userId of roomMembers) {
+					if (user.id === userId) {
+						return user;
+					}
+				}
+			});
+
+			// retrieve messages from users in room
+			const messages = db.getMany("messages", function (message) {
+				return message.roomId === roomId;
+			}).map(function (message) {
+				const user = users.find(function (user) {
+					return user.id === message.userId;
+				});
+				message.user = user;
+				delete message.userId;
+				return message;
+			});
+
 			// socket.to(room.name).emit("messages", messages);
 			socket.emit("messages", messages);
 		});
 
 		socket.on("disconnect", function () {
 			console.log("client disconnected");
+			// remove client from clients list
+			clients.splice(clients.findIndex(function (client) {
+				return client.id === socket.id;
+			}, 1));
 		});
 	});
 };
