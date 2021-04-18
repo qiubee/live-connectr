a6a05acbb4db964d4792d634365befca9649ef4a
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-18 20:29:28 +0200

    add Journey view

---
+++ client/src/views/Journey.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="home">
+		<Room/>
+	</div>
+</template>
+
+<script>
+import Room from "@/components/Room.vue";
+
+
+export default {
+	name: "Journey",
+	components: {
+		Room
+	}
+};
+</script>
