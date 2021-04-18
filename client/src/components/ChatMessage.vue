a801ec1804516cd7151e5251ef8fdfb417f2adeb
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-18 20:28:10 +0200

    show message time

--- client/src/components/ChatMessage.vue
+++ client/src/components/ChatMessage.vue
@@ -1,11 +1,9 @@
 <template>
 	<li>
-		<div class="message">
+		<div class="message" :class="{sender: user.sender}">
+			<p>{{message}}</p>
 			<div>
-				<p>{{message}}</p>
-			</div>
-			<div>
-				<span>{{user.name}}</span>
+				<span v-if="!user.sender">{{user.name}}</span>
 				<span>
 					<time v-bind:datetime="datetime">{{timeString}}</time>
 				</span>
@@ -19,16 +17,21 @@ export default {
 	name: "ChatMessage",
 	props: {
 		user: Object,
-		socket: Object,
 		message: String,
 		datetime: String
 	},
 	data() {
 		return {
-			timeString: ""
+			timeString: String
 		};
 	},
 	mounted() {
+		const datetime = this.datetime;
+		const hours = new Date(datetime).getHours();
+		const minutes = new Date(datetime).getMinutes();
+		const fullHours = hours === 0 ? 24 : hours < 10 ? "0" + hours : hours;
+		const fullMinutes = minutes < 10 ? "0" + minutes : minutes;
+		this.timeString = `${fullHours}:${fullMinutes}`;
 	}
 };
 </script>
