402bd080518ead379f71218a6b2e69e04acc9c99
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-18 20:20:27 +0200

    update about route & add journey route

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -6,14 +6,17 @@ const routes = [
 		path: "/",
 		name: "Home",
 		component: Home
-	},
-	{
-		path: "/about",
-		name: "About",
+	}, {
+		path: "/over-zugo",
+		name: "Over",
 		// route level code-splitting
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
-		component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
+		component: () => import(/* webpackChunkName: "over" */ "../views/Over.vue")
+	}, {
+		path: "/reis/:id",
+		name: "Journey",
+		component: () => import(/* webpackChunkName: "journey" */ "../views/Journey.vue")
 	}
 ];
 
