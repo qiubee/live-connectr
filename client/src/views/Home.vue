1f75f69f9bce3ca61064e82fcf1e7f01b0b5a902
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-19 21:55:57 +0200

    add SearchJourney component to homepage

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
 	<div class="home">
+		<SearchJourney/>
 		<Room/>
+		
 	</div>
 </template>
 
 <script>
 import Room from "@/components/Room.vue";
+import SearchJourney from "@/components/SearchJourney.vue";
 
 
 export default {
 	name: "Home",
 	components: {
-		Room
+		Room,
+		SearchJourney
 	}
 };
 </script>
