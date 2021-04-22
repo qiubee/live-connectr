cf6ef8aa4430e0042cb668792ec0fa668a0c868d
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-22 22:44:46 +0200

    add get & post room(s) & update api paths

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 
 const api = require("./controllers/api");
 
-router.get("/api/v1/journey", api.journey)
-	.get("/api/v1/search", api.search)
-	.get("/api/v1/stations", api.stations)
-	.get("/api/v1/train", api.train);
+router.get("/api/v1/journeyDetail", api.getJourneyInfo)
+	.get("/api/v1/journeys", api.getJourneys)
+	.get("/api/v1/stations", api.getStations)
+	.get("/api/v1/trainDetail", api.getTrainInfo)
+	.get("/api/v1/allRooms", api.getAllRooms)
+	.get("/api/v1/room", api.getRoom)
+	.post("/api/v1/room", api.postRoom);
 
 module.exports = router;
