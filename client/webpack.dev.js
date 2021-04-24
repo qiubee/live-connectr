f6b75755fd3065bac4c1e13b8e4d2ac54f6904dc
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-24 17:02:43 +0200

    add webpack dev config & merge with common config

---
+++ client/webpack.dev.js
@@ -0,0 +1,27 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
+
+module.exports = merge(common, {
+	mode: "development",
+	devtool: "inline-source-map",
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			},
+		]
+	},
+	devServer: {
+		hot: true,
+		stats: "minimal",
+		inline: true,
+		port: 5000,
+		contentBase: path.resolve(__dirname, "dist"),
+		publicPath: "/",
+		watchContentBase: true,
+		quiet: true,
+		clientLogLevel: "warn"
+	}
+});
