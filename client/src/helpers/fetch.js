523e5166fc117aa490a1c779d8662678439e8516
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-25 20:53:52 +0200

    add fetch module to get & post data

---
+++ client/src/helpers/fetch.js
@@ -0,0 +1,32 @@
+import { get as aGet, post as aPost } from "axios";
+async function get(url, params = null) {
+	try {
+		if (params) {
+			return await (await aGet(url, { params: params })).data;
+		} else {
+			return await (await aGet(url)).data;
+		}
+	} catch (error) {
+		if (error.response.status >= 400 && error.response.status < 500) {
+			console.error(error.response.status + " " + error.response.data.message);
+			return error.response;
+		} else {
+			console.error(error);
+		}
+	}
+}
+
+async function post(url, data) {
+	try {
+		return await (await aPost(url, data));
+	} catch (error) {
+		if (error.response.status >= 400 && error.response.status < 500) {
+			console.error(error.response.status + " " + error.response.data.message);
+			return error.response;
+		} else {
+			console.error(error);
+		}
+	}
+}
+
+export { get, post };
