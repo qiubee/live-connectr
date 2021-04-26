bb7a1e99708b153733fcd5e25531218270cfa01c
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-26 21:45:18 +0200

    add GeneralJourneyInfo component

---
+++ client/src/components/GeneralJourneyInfo.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="journey_info">
+		<div class="category">
+			<span class="type" v-if="altTypeName">{{altTypeName}}</span>
+			<span class="type" v-else>{{type.name}}</span>
+			<span class="operator" :class="operator.toLowerCase()"></span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "GeneralJourneyInfo",
+	props: {
+		destination: String,
+		nextStop: String,
+		delayedInSeconds: Number,
+		operator: String,
+		type: Object,
+		currentPlace: String
+	}
+};
+</script>
+
+<style scoped>
+
+</style>
