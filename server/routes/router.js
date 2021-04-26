137bd982ee063868abf6acb1d7245f9f33b955b7
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-26 21:29:14 +0200

    update get paths

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 
 const api = require("./controllers/api");
 
-router.get("/api/v1/journeyDetail", api.getJourneyInfo)
+router.get("/api/v1/journey", api.getJourneyInfo)
 	.get("/api/v1/journeys", api.getJourneys)
 	.get("/api/v1/stations", api.getStations)
-	.get("/api/v1/trainDetail", api.getTrainInfo)
+	.get("/api/v1/train", api.getTrainInfo)
 	.get("/api/v1/allRooms", api.getAllRooms)
 	.get("/api/v1/room", api.getRoom)
 	.get("/api/v1/user", api.getUser)
