af6819c7a39fff9e84b6919d84375511ac1af897
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-27 22:17:39 +0200

    fix cors missing origin & update api route

--- server/server.js
+++ server/server.js
@@ -10,19 +10,19 @@ const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 
+// allow localhost origin in development
+if (process.env.NODE_ENV === "development") {
+	app.use("/api/", cors({origin: "http://localhost:5000"}));
+}
+
 app.use(express.static("public")) // use public folder for static files
-	.use(express.urlencoded({extended: true})) // get data from http body
+	.use(express.urlencoded({extended: true})) // parse data from http body
 	.use(express.json()) // parse json body
-	.use("/", router) // use router
+	.use("/api/", router) // use api router
 	.use(function (req, res) {
-		res.redirect("/"); // redirect to home  on 404
+		res.redirect("/"); // redirect to home on 404
 	})
 	.disable("x-powered-by");
-
-if (process.env.NODE_ENV === "development") {
-	// allow localhost origin in development
-	app.use(cors({origin: "http://localhost:5000"}));
-}
 
 // socket implementation
 socket(server); 
