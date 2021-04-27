b96b85a084c812fbdf991636753b48c8ad35caac
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-27 22:18:04 +0200

    update api path names

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 
 const api = require("./controllers/api");
 
-router.get("/api/v1/journey", api.getJourneyInfo)
-	.get("/api/v1/journeys", api.getJourneys)
-	.get("/api/v1/stations", api.getStations)
-	.get("/api/v1/train", api.getTrainInfo)
-	.get("/api/v1/allRooms", api.getAllRooms)
-	.get("/api/v1/room", api.getRoom)
-	.get("/api/v1/user", api.getUser)
-	.post("/api/v1/room", api.postRoom)
-	.post("/api/v1/user", api.postUser);
+router.get("/v1/journey", api.getJourneyInfo)
+	.get("/v1/journeys", api.getJourneys)
+	.get("/v1/stations", api.getStations)
+	.get("/v1/train", api.getTrainInfo)
+	.get("/v1/allRooms", api.getAllRooms)
+	.get("/v1/room", api.getRoom)
+	.get("/v1/user", api.getUser)
+	.post("/v1/room", api.postRoom)
+	.post("/v1/user", api.postUser);
 
 module.exports = router;
