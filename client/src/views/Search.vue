1f6511fa2885111243c423080802e0b6d0f0a7ee
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-28 13:49:41 +0200

    add Search view & add SearchJourney component

---
+++ client/src/views/Search.vue
@@ -0,0 +1,21 @@
+<template>
+	<SearchJourney :socket="socket"/>
+</template>
+
+<script>
+import SearchJourney from "@/components/SearchJourney.vue";
+
+export default {
+	name: "Search",
+	components: {
+		SearchJourney
+	},
+	props: {
+		socket: Object
+	}
+};
+</script>
+
+<style>
+
+</style>
