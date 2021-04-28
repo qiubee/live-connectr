6e8784cb228248b6b616de164eb5dd81939065bb
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-28 13:56:55 +0200

    fix rooms data & add RoomItem component

--- client/src/components/RoomList.vue
+++ client/src/components/RoomList.vue
@@ -1,53 +1,61 @@
 <template>
-	<div class="current_rooms" v-if="!noRooms">
+	<div class="all_rooms" v-if="rooms.length >= 1">
 		<ul>
-			<li v-for="room in rooms" :key="room.id">
-				<div class="active_room">
-					{{room}}
-				</div>
+			<li class="room" v-for="room in rooms" :key="room.id">
+				<RoomItem @click="toRoom(room.journey.journeyId)"
+					:journey="room.journey"
+					:totalMembers="room.users.length"
+					:game="room.game"
+				/>
 			</li>
 		</ul>
 		<div>
 			<p>Zit jouw reis er niet bij?</p>
-			<button>Zoek reis</button>
+			<router-link to="/zoek-reis">Zoek reis</router-link>
 		</div>
 	</div>
 	<div class="no_rooms" v-else>
 			<p>Er zijn op dit moment geen personen die reizen. Zoek jouw eigen reis en speel mini-games totdat anderen meereizen.</p>
-			<button>Zoek reis</button>
+			<router-link to="/zoek-reis">Zoek reis</router-link>
 		</div>
 </template>
 
 <script>
+import RoomItem from "@/components/RoomItem.vue";
+
+
 export default {
 	name: "RoomList",
 	props: {
 		socket: Object
 	},
+	components: {
+		RoomItem
+	},
 	data() {
 		return {
-			rooms: Array,
-			noRooms: true
+			rooms: [],
 		};
 	},
 	created() {
+		const vm = this;
 		const socket = this.socket;
 		socket.emit("all rooms", {init: true});
 		
 		socket.on("all rooms", function (rooms) {
-			if (rooms) {
-				if (rooms.length > 0) {
-					this.noRooms = false;
-				} else {
-					this.noRooms = true;
-				}
-				this.rooms = rooms;
-			}
+			vm.rooms = rooms;
 		});
+	},
+	methods: {
+		toRoom(id) {
+			this.$router.push({name: "Journey", params: {id}});
+		}
 	}
 };
 </script>
 
-<style>
-
+<style scoped>
+.all_rooms .room:hover {
+	cursor: pointer;
+}
 </style>
